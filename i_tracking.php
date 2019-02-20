a2bb9dabf52bc17d336bb43e56edbef55fa12c13
Bagus Javaneze <myname.isgood@hotmail.com>
2019-02-20 14:55:46 +0700

    simplify code + update design

--- i_tracking.php
+++ i_tracking.php
@@ -1,213 +1,92 @@
 <?php
-sleep(1);
+
 
 $resi = $_POST['resi'];
 $jasa = $_POST['jasa'];
-
 
 
 //jika jne
 if ($jasa == "jne")
 	{
 	$base = "https://track.aftership.com/jne/$resi?";
-	
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($curl, CURLOPT_HEADER, false);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($curl, CURLOPT_URL, $base);
-	curl_setopt($curl, CURLOPT_REFERER, $base);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	$str = curl_exec($curl);
-	curl_close($curl);
-	
-	
-	
-	
-	echo "<h1>$jasa</h1>";
-	
-	
-	
-	
-	
-	//sempurnakan
-	$start = "<div class=\"checkpoints\">";
-	$end   = "Date & time are usually";
-	$startPosisition = strpos($str, $start);
-	$endPosisition   = strpos($str, $end); 
-	
-	$longText = $endPosisition - $startPosisition;
-	
-	$result = substr($str, $startPosisition, $longText);
-		
-	
-	
-	
-	
-	//hilangkan
-	function hilangkan($str)
-		{
-	    $str = trim($str);
-		$search = array ("'Indonesia'");
-		$replace = array ("");
-	
-		$str = preg_replace($search,$replace,$str);
-		return $str;
-	  	}
-	
-	
-	
-	$result2 = hilangkan($result);
-	
-	
-	//jika salah
-	if (empty($result2))
-		{
-		echo "SALAH, HARAP CEK LAGI...";
-		}
-	else
-		{
-		echo $result2;
-		}
 	}
-	
-	
-	
-	
-	
 	
 	
 //jika pos
 if ($jasa == "pos")
 	{
 	$base = "https://track.aftership.com/pos-indonesia/$resi?";
-	
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($curl, CURLOPT_HEADER, false);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($curl, CURLOPT_URL, $base);
-	curl_setopt($curl, CURLOPT_REFERER, $base);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	$str = curl_exec($curl);
-	curl_close($curl);
-	
-	
-	
-	
-	echo "<h1>$jasa</h1>";
-	
-	
-	//sempurnakan
-	$start = "<div class=\"checkpoints\">";
-	$end   = "Date & time are usually";
-	$startPosisition = strpos($str, $start);
-	$endPosisition   = strpos($str, $end); 
-	
-	$longText = $endPosisition - $startPosisition;
-	
-	$result = substr($str, $startPosisition, $longText);
-		
-	
-	
-	
-	
-	//hilangkan
-	function hilangkan($str)
-		{
-	    $str = trim($str);
-		$search = array ("'Indonesia'");
-		$replace = array ("");
-	
-		$str = preg_replace($search,$replace,$str);
-		return $str;
-	  	}
-	
-	
-	
-	$result2 = hilangkan($result);
-	
-	
-	
-	//jika salah
-	if (empty($result2))
-		{
-		echo "SALAH, HARAP CEK LAGI...";
-		}
-	else
-		{
-		echo $result2;
-		}
 	}
-	
-	
-	
 	
 	
 //jika tiki
 if ($jasa == "tiki")
 	{
 	$base = "https://track.aftership.com/tiki/$resi?";
-	
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($curl, CURLOPT_HEADER, false);
-	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($curl, CURLOPT_URL, $base);
-	curl_setopt($curl, CURLOPT_REFERER, $base);
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-	$str = curl_exec($curl);
-	curl_close($curl);
-	
-	
-	
-	
-	echo "<h1>$jasa</h1>";
-	
-
-	//sempurnakan
-	$start = "<div class=\"checkpoints\">";
-	$end   = "Date & time are usually";
-	$startPosisition = strpos($str, $start);
-	$endPosisition   = strpos($str, $end); 
-	
-	$longText = $endPosisition - $startPosisition;
-	
-	$result = substr($str, $startPosisition, $longText);
-		
-	
-	
-	
-	
-	//hilangkan
-	function hilangkan($str)
-		{
-	    $str = trim($str);
-		$search = array ("'Indonesia'");
-		$replace = array ("");
-	
-		$str = preg_replace($search,$replace,$str);
-		return $str;
-	  	}
-	
-	
-	
-	$result2 = hilangkan($result);
-	
-	
-	
-	//jika salah
-	if (empty($result2))
-		{
-		echo "SALAH, HARAP CEK LAGI...";
-		}
-	else
-		{
-		echo $result2;
-		}
 	}
 	
-	
 
-exit();
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_URL, $base);
+curl_setopt($curl, CURLOPT_REFERER, $base);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+$str = curl_exec($curl);
+curl_close($curl);
+
+//sempurnakan
+$start = "<div class=\"checkpoints\">";
+$end   = "Date & time are usually";
+$startPosisition = strpos($str, $start);
+$endPosisition   = strpos($str, $end); 
+
+$longText = $endPosisition - $startPosisition;
+
+$result = substr($str, $startPosisition, $longText);	
+
+//hilangkan
+function hilangkan($str)
+{
+    $str = trim($str);
+	$search = array ("'Indonesia'");
+	$replace = array ("");
+
+	$str = preg_replace($search, $replace, $str);
+	return $str;
+}
+
+//bikin file html
+function bikinfile($text, $filename='hasil_resi.html')
+{
+	if (!file_exists(realpath('./cache'))) {
+	   mkdir(dirname(__FILE__).'/cache', 0775, true);
+	}
+	
+	//tulis text ke file
+	$myfile = fopen(realpath('./cache').'/'.$filename, "w") or die("Unable to open file!");
+	fwrite($myfile, $text);
+	return fclose($myfile);
+}
+
+
+$finalResult = hilangkan($result);
+$finalString = preg_replace("/\r|\n/", "", $finalResult);
+$data = bikinfile($finalString);
+
+
+//jika salah
+if ($data && !empty($finalString))
+{
+	echo json_encode([
+		'write_status' => $data
+	]);
+}
+else
+{
+	echo json_encode(["EMPTY"]);
+}
+
+
 ?>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@ error_reporting(0);
 <title>CEK RESI JNE, POS & TIKI</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.css" rel="stylesheet">
 
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
@@ -26,41 +27,42 @@ error_reporting(0);
 
 <script type="text/javascript">
 $(document).ready(function() {
-	$('#loading').ajaxStart(function(){
-			$(this).show();
-		}).ajaxStop(function(){
-			$(this).hide();
-		});
 
 
+$("#btn-cek").on('click', function(e){
+	e.preventDefault();
 
-
-
-
-
-$("#cek").on('click', function(){
-
-	
-	$("#iloading").show();
-				
-	$("#formku2").submit(function(){
-		
-		$.ajax({
-			url: "i_tracking.php",
-			type:"POST",
-			data:$(this).serialize(),
-			success:function(data){			
-				$("#ihasil").html(data);
-				$("#iloading").hide();
-				}
-			});
+	if($("#resi").val().trim()==="" || $("#jasa").val()==="") 
+	{
+		alert('Semua field harus diisi!');
 		return false;
+	}		
+
+	$("#iloading").show();
+	var nomor_resi = $("#resi").val().trim();
+	var jasa_kurir = $("#jasa").val();
+
+	$.ajax({
+		url: "i_tracking.php",
+		type: "POST",
+		data: {resi: nomor_resi, jasa: jasa_kurir},
+		dataType: "json",
+		cache: false,
+		success:function(data){
+			if(data['write_status'] != undefined){
+				var iframetag = '<iframe id="result_iframe" src="cache/hasil_resi.html" style="width: 100%;height: 900px;border: 2px solid #eee;"></iframe>';
+				$("#ihasil").html(iframetag);
+			}
+			else{
+				var gagal = '<div class="alert alert-danger"><strong>NOMOR RESI TIDAK VALID!</strong><br>HARAP PERIKSA KEMBALI NOMOR RESI SESUAI JASA KURIR YANG DIPILIH.</div>';
+				$("#ihasil").html(gagal);
+			}
+			$("#iloading").hide();
+		}
 	});
+	return false;
 
 });	
-
-
-
 
 
 $("#iloading").hide();
@@ -70,37 +72,60 @@ $("#iloading").hide();
 
 </script>
 
-      
-      
+<style type="text/css">
+	.m-t-10 {
+		margin-top: 10px;
+	}
+	.m-b-10 {
+		margin-bottom: 10px;
+	}
+</style>
+
 </head>
 <body>
 
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-lg-12">
+			<h1>CEK RESI JNE, POS & TIKI</h1>
+		</div>
+	</div>
+</div>			
 
-
-<h1>CEK RESI JNE, POS & TIKI</h1>
-
-
-<form class="search" method="POST" name="formku2" id="formku2">
-<input class="search-input" type="search" name="resi" id="resi" placeholder="Nomor Resi..." value="" autofocus="" required="">
-<select name="jasa" id="jasa" class="search-select" required>
-		<option value="jne">JNE</option>
-		<option value="pos">POS</option>
-		<option value="tiki">TIKI</option>
-
-</select>
-<button class="search-submit" type="submit" name="cek" id="cek">Cek Resi</button>
-</form>
-				
-
-
-<div id="iloading">
-	<img src="progress-bar.gif" width="100" height="16">loading...
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-lg-12">
+			<form method="POST" name="formku2" id="formku2">
+				<div class="form-group">
+			      <label for="nomor-resi">Nomor Resi: <span class="text-danger"><strong>*</strong></span></label>
+			      <input class="form-control" type="search" name="resi" id="resi" placeholder="Nomor Resi..." value="" autofocus="true" required>
+			    </div>
+			    <div class="form-group">
+			      <label for="jasa">Jasa Kurir: <span class="text-danger"><strong>*</strong></span></label>
+			      <select name="jasa" id="jasa" class="form-control" required>
+			        <option value="" selected>&mdash; PILIH JASA KURIR &mdash;</option>
+			        <option value="jne">JNE</option>
+			        <option value="pos">POS</option>
+			        <option value="tiki">TIKI</option>  
+			      </select>
+			    </div>
+				<p><span class="text-danger"><strong>*</strong></span>) wajib diisi</p>
+			</form>
+			<div class="btn btn-primary" id="btn-cek">Cek Resi</div>
+			<div class="m-t-10 m-b-10" id="iloading">
+				<img src="progress-bar.gif" width="100" height="16">loading...
+			</div>
+		</div>
+	</div>
 </div>
 
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 col-lg-12">
+			<div class="m-t-10 m-b-10" id="ihasil"></div>
+		</div>
+	</div>
+</div>	
 
-<div id="ihasil"></div>
-
-
-
-
-</body></html>
+</body>
+</html>
